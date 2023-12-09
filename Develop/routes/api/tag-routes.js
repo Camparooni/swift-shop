9794009889c35415545152733b16b15da2f97971
Tay Campanelli <tayascamp@yahoo.com>
2023-12-08 19:17:25 -0800

    Added tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,24 +6,87 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  try {
+    const tags = await Tag.findAll ({
+      include: [{ model: Product, through: ProductTag }],
+    });
+    res.json(tags);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  try {
+    const tagId = req.params.id;
+    const tag = await Tag.findByPk(tagId, {
+      include: [{ model: Product, through: ProductTag }],
+    });
+
+    if(!tag) {
+      res.status(404).json({ error: 'Tag not found' });
+      return;
+    }
+
+    res.json(tag);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  try {
+    const { tag_name } = req.body;
+    const newtag = await Tag.create({ tag_name });
+    res.json(newTag);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagId = req.params.id;
+    const { tag_name } = req.body;
+
+    const tag = await Tag.findByPk(tagId);
+    if (!tag) {
+      res.status(404).json({ error: 'Tag not found' });
+      return;
+    }
+
+    await tag.update({ tag_name });
+
+    res.json(tag);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagId = req.params.id;
+    const tag = await Tag.findbyPk(tagId);
+    
+    if(!tag) {
+      res.status(404).json({ error: 'Tag not found' });
+      return;
+    }
+
+    await tag.destroy();
+    res.json({ message: 'Tag deleted successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+  }
 });
 
 module.exports = router;
